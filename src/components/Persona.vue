ebba94afdcbdfec4ef23a9354ec6b071fcf135cd
Sebastian Zapata <pansezapata@gmail.com>
2021-09-13 13:47:27 -0500

    Se actualiza el proyecto

--- src/components/Persona.vue
+++ src/components/Persona.vue
@@ -3,6 +3,8 @@
         <strong>Nombre: </strong>{{persona.nombre}} <br/>
         <strong>Apellido: </strong>{{persona.apellido}}<br/>
         <strong>Edad: </strong> {{persona.edad}}
+        <p v-if="persona.edad>=20"> Mandelo a trabajar </p>
+        <p v-else>Siga estudiando</p>
     </div>   
 </template>
 <script>
--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -3,16 +3,26 @@
         <h1>Soy el componente Principal</h1>
         <h2>{{nombre}}</h2><br/>
         <input type="text"
-        v-model="nombre" />
+        v-model="nombre" /><br/>
+        <div v-for="persona in estudiantes" :key="persona.id">
+            <persona :persona="persona"/>
+        </div>
     </div>    
 </template>
 <script>
+import Persona from './Persona.vue'
  export default{
+  components: { Persona },
      name:"Principal",
      data(){
          return{
              nombre:'',
-             edad:0
+             estudiantes:[
+                {id:1,nombre:'Lorena',apellido:'Lopez',edad:19},
+                {id:2,nombre:'Juan David',apellido:'SantaMaria',edad:22},
+                {id:3,nombre:'Juan Pablo',apellido:'Herrera',edad:18},
+                {id:4,nombre:'Alexi',apellido:'Lopez',edad:19},
+                ]
          }
          
      }
